aab267fc56c011e402966f74e1da302034b9d7d5
Miroslav Smiljanic <smiljani@adobe.com>
2023-03-31 17:16:47 +0200

    fix: lint js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -98,7 +98,7 @@ function toggleAllNavSections(sections, expanded = false) {
 function toggleMenu(nav, navSections, forceExpanded = null) {
   const expanded = forceExpanded !== null ? !forceExpanded : nav.getAttribute('aria-expanded') === 'true';
   const button = nav.querySelector('.nav-hamburger button');
-  
+
   nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
 
   button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
