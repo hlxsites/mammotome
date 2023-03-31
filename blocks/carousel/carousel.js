7bea16ea2ebded8d970f56273fc1c4cd0cd7ea9d
Acidham <acidham@gmail.com>
2023-03-31 13:24:42 +0200

    improve LHS

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -8,6 +8,7 @@ const HTML_ARROW_LEFT = '<svg fill="rgba(238, 238, 238, 0.9)" xmlns="http://www.
 const HTML_ARROW_RIGHT = '<svg fill="rgba(238, 238, 238, 0.9)" xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 600 600">\n'
   + '<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"/>\n'
   + '</svg>\n';
+
 const activateBullet = (bulletId) => {
   const btnNav = document.querySelector('.bottom-nav');
   [...btnNav.children].forEach((el) => {
@@ -65,7 +66,7 @@ const arrowNavOnClickEvents = () => {
 };
 
 // Control Slide show
-const slideShowControl = () => {
+const toNextSlide = () => {
   const nextSlide = (activeSlide + 1) <= slideCount ? activeSlide + 1 : 1;
 
   activateSlide(`slide-${nextSlide}`);
@@ -76,7 +77,7 @@ const slideShowControl = () => {
 // Start slide show
 const startSlideShow = () => {
   if (!slideShow) {
-    slideShow = window.setInterval(slideShowControl, 5000);
+    slideShow = window.setInterval(toNextSlide, 5000);
   }
 };
 
