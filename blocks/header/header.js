d4ef5b21dad1893e910a86b0de834c5fd83e2806
Miroslav Smiljanic <smiljani@adobe.com>
2023-03-31 14:13:31 +0200

    fix: dont set overflowY on body

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -98,7 +98,7 @@ function toggleAllNavSections(sections, expanded = false) {
 function toggleMenu(nav, navSections, forceExpanded = null) {
   const expanded = forceExpanded !== null ? !forceExpanded : nav.getAttribute('aria-expanded') === 'true';
   const button = nav.querySelector('.nav-hamburger button');
-  document.body.style.overflowY = (expanded || isDesktop.matches) ? '' : 'hidden';
+  
   nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
 
   button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
