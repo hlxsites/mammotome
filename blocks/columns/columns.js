022f3ae2c226044701713ba26428d18319d960ae
Francisco Chicharro Sanz <chicharr@adobe.com>
2023-04-12 14:19:56 +0200

    Columns first commit

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -7,11 +7,21 @@ export default function decorate(block) {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
-        const picWrapper = pic.closest('div');
+        const picDecorationWrapper = pic.closest('div');
+        picDecorationWrapper.classList.add('columns-img-wrapper');
+        picDecorationWrapper.classList.add('columns-img-decoration');
+        const picWrapper = document.createElement('div');
+        picWrapper.appendChild(pic);
+        picDecorationWrapper.appendChild(picWrapper);
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
         }
+      }
+      const text = col.querySelector('p');
+      if (text) {
+        const textWrapper = text.closest('div');
+        textWrapper.classList.add('columns-txt-wrapper');
       }
     });
   });
