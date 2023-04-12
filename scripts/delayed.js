ae27afa73abf525e72cf8846d305cea8bfd269db
Tobias Reiss <reiss@adobe.com>
2023-04-12 17:35:52 +0200

    quick test: do not delay martech

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,10 +1,8 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM, fetchPlaceholders } from './lib-franklin.js';
-import integrateMartech from './third-party.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-integrateMartech();
 
 // add more delayed functionality here
 fetchPlaceholders(`/${window.location.pathname.split('/')[1]}`);
