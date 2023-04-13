a2973d04311a6a1264dab25f32e7693c6b4243de
Tobias Reiss <reiss@adobe.com>
2023-04-13 16:39:06 +0200

    test: nice js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,6 +22,8 @@ import {
   observeHistorySection,
 } from './lib-history-section.js';
 
+import integrateMartech from './third-party.js';
+
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 window.hlx.RUM_GENERATION = 'mammotome'; // add your RUM generation information here
 
@@ -175,6 +177,8 @@ async function loadLazy(doc) {
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
+
+  integrateMartech();
 
   // Load experimentation preview overlay
   if (window.location.hostname === 'localhost' || window.location.hostname.endsWith('.hlx.page')) {
--- scripts/third-party.js
+++ scripts/third-party.js
@@ -1,17 +1,18 @@
-// Google Tag Manager
-const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KNBZTHP');`;
+function createInlineScript(parent, id) {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'gtm.start': new Date().getTime(),
+    event: 'gtm.js',
+  });
 
-function createInlineScript(innerHTML, parent) {
+  // Google Tag Manager
   const script = document.createElement('script');
   script.type = 'text/javascript';
-  script.innerHTML = innerHTML;
+  script.src = `https://www.googletagmanager.com/gtm.js?id=${id}`;
+  script.async = true;
   parent.appendChild(script);
 }
 
 export default function integrateMartech() {
-  createInlineScript(GTM_SCRIPT, document.body);
+  createInlineScript(document.body, 'GTM-KNBZTHP');
 }
