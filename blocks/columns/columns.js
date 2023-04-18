6d6b36505a3e970cf49e56efce6403f0ffec3be8
Francisco Chicharro Sanz <chicharr@adobe.com>
2023-04-18 13:22:43 +0200

    27 - columns first commit

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -3,23 +3,29 @@ export default function decorate(block) {
   block.classList.add(`columns-${cols.length}-cols`);
 
   // setup image columns
-  [...block.children].forEach((row) => {
+  [...block.children].forEach((row, index) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
-      if (pic) {
-        const picDecorationWrapper = pic.closest('div');
-        picDecorationWrapper.classList.add('columns-img-wrapper');
-        picDecorationWrapper.classList.add('columns-img-decoration');
-        const picWrapper = document.createElement('div');
-        picWrapper.appendChild(pic);
-        picDecorationWrapper.appendChild(picWrapper);
+      const text = col.querySelector('p');
+
+      if (pic && !text) {
+        const oddeven = (index % 2 === 0) ? 'even' : 'odd';
+        const picWrapper = pic.closest('div');
+        picWrapper.classList.add('columns-img-wrapper');
+        picWrapper.classList.add(`columns-img-wrapper-${oddeven}`);
+        const picDecoration = document.createElement('div');
+        picDecoration.classList.add('columns-img-decoration');
+        const picImg = document.createElement('div');
+        picImg.classList.add('columns-img');
+        picImg.appendChild(pic);
+        picDecoration.appendChild(picImg);
+        picWrapper.appendChild(picDecoration);
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
         }
       }
-      const text = col.querySelector('p');
-      if (text) {
+      if (text && !pic) {
         const textWrapper = text.closest('div');
         textWrapper.classList.add('columns-txt-wrapper');
       }
