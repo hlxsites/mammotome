4a1200f7faebc30da826b9c2a297863263d690ad
Acidham <acidham@gmail.com>
2023-04-18 13:20:51 +0200

    add contact/demo button

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -302,7 +302,7 @@ export default async function decorate(block) {
 
     const navSections = nav.querySelector('.nav-sections');
     if (navSections) {
-      navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
+      navSections.querySelectorAll(':scope > ul > li').forEach((navSection, i) => {
         if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
         if (navSection.querySelector('ul > li > ul > li > ul')) navSection.classList.add('nav-multilevel');
 
@@ -310,6 +310,7 @@ export default async function decorate(block) {
         if (navList) navList.prepend(createMobileMenuControlsBlock());
         else {
           navSection.classList.add('nav-button');
+          if (i % 2) navSection.querySelector('a').classList.add('btn-invert');
         }
 
         navSection.addEventListener('click', () => {
