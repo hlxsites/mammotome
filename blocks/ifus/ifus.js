55eefd29d0a825adeffe36d9458943f46a98ccb7
Karl Pauls <karlpauls@gmail.com>
2023-04-20 09:47:37 +0200

    Second

--- blocks/ifus/ifus.js
+++ blocks/ifus/ifus.js
@@ -18,36 +18,54 @@ function buildTags(tags) {
     if (tag.target) {
       element.target = tag.target;
     }
+    if (tag.class) {
+      element.classList.add(tag.class);
+    }
+    if (tag.classes) {
+      tag.classes.forEach((c) => element.classList.add(c));
+    }
+    if (tag.id) {
+      element.id = tag.id;
+    }
     return element;
   });
 }
 
-function populateIFU(json, block) {
-  json.eIFUs.data.forEach((eIFU) => {
+function populateSearch(
+  idSelector,
+  countrySelector,
+  searchButton,
+  result,
+  idProducer,
+  countryProducer,
+  assetFilter,
+  productCodes,
+  clear,
+) {
+  idProducer().forEach((value) => {
     const option = document.createElement('option');
-    option.text = eIFU.ID;
-    block.querySelector('#eIFU-code').append(option);
+    option.text = value;
+    idSelector.append(option);
   });
-  const eIFU = block.querySelector('#eIFU-code');
-  eIFU.disabled = false;
-  json.countries.data.forEach((country) => {
+  idSelector.disabled = false;
+
+  countryProducer().forEach((country) => {
     const option = document.createElement('option');
     option.text = country['Country Name'];
     option.value = country.ISO_3166_1_alpha_2_code;
-    block.querySelector('#eIFU-country').append(option);
+    countrySelector.append(option);
   });
-  const eIFUCountry = block.querySelector('#eIFU-country');
-  eIFUCountry.disabled = false;
+  countrySelector.disabled = false;
 
-  const search = block.querySelector('#eIFU-code-search');
-  search.disabled = false;
-  search.addEventListener('click', async () => {
-    const assets = json.IFUAssets.data.filter((asset) => asset.eIFU === eIFU.value
-      && asset.Country === eIFUCountry.value);
-    block.querySelector('.ifu-result').innerHTML = '';
-    block.querySelector('.ifu-result').append(...buildTags([{ name: 'h4', text: await translate('ifuSearchTitle', 'Search results') }]));
+  searchButton.disabled = false;
+  searchButton.addEventListener('click', async () => {
+    clear.forEach((value) => { value.options[0].selected = true; });
+    const assets = idSelector.value && countrySelector.value
+      ? assetFilter(idSelector, countrySelector) : [];
+    result.innerHTML = '';
+    result.append(...buildTags([{ name: 'h4', text: await translate('ifuSearchTitle', 'Search results') }]));
     if (assets.length === 0) {
-      block.querySelector('.ifu-result').append(...buildTags([{ name: 'div', next: [{ name: 'strong', text: await translate('ifuSearchNoResult', 'No result') }] }]));
+      result.append(...buildTags([{ name: 'div', next: [{ name: 'strong', text: await translate('ifuSearchNoResult', 'No result') }] }]));
     } else {
       const map = new Map();
       assets.forEach((asset) => {
@@ -73,7 +91,7 @@ function populateIFU(json, block) {
                         [
                           {
                             name: 'div',
-                            text: json.eIFUToProductCodes.data.filter((match) => match.eIFU_ID === eIFU.value).map((match) => match['Product Code']).join(', '),
+                            text: productCodes(idSelector),
                             first:
                               [
                                 { name: 'h6', text: await translate('ifuSearchProductCodes', 'Product Code(s)') },
@@ -83,7 +101,7 @@ function populateIFU(json, block) {
                     },
                     {
                       name: 'div',
-                      text: eIFUCountry.value,
+                      text: countrySelector.value,
                       first:
                         [
                           { name: 'h6', text: await translate('ifuSearchCountrySelected', 'Country Selected') },
@@ -102,128 +120,10 @@ function populateIFU(json, block) {
             },
           ],
         );
-        block.querySelector('.ifu-result').append(...entry);
+        result.append(...entry);
       }));
     }
-    block.querySelector('.ifu-result').classList.remove('no-result');
-  });
-}
-
-function populateUDI(json, block) {
-  const udi = block.querySelector('#udi-code');
-  json.UDIs.data.forEach((value) => {
-    const option = document.createElement('option');
-    option.text = value.Code;
-    udi.append(option);
-  });
-  udi.disabled = false;
-
-  const udiCountry = block.querySelector('#udi-country');
-  json.countries.data.forEach((country) => {
-    const option = document.createElement('option');
-    option.text = country['Country Name'];
-    option.value = country.ISO_3166_1_alpha_2_code;
-    udiCountry.append(option);
-  });
-  udiCountry.disabled = false;
-
-  const search = block.querySelector('#udi-code-search');
-  search.disabled = false;
-  search.addEventListener('click', () => {
-    const assets = json.eIFUToUDIs.data.filter((entry) => entry.UDI_DI === udi.value)
-      .flatMap((entry) => json.IFUAssets.data.filter((asset) => asset.eIFU === entry.eIFU
-        && asset.Country === udiCountry.value));
-    if (assets.length === 0) {
-      block.querySelector('.ifu-result').innerHTML = '<h4>Search results</h4><div><strong>No result</strong></div>';
-    } else {
-      block.querySelector('.ifu-result').innerHTML = '<h4>Search results</h4>';
-      const map = new Map();
-      assets.forEach((asset) => {
-        if (map.has(asset.Title)) {
-          const links = map.get(asset.Title);
-          links.push(asset.URL);
-        } else {
-          map.set(asset.Title, [asset.URL]);
-        }
-      });
-      map.forEach((value, key) => {
-        const entry = document.createElement('div');
-        entry.innerHTML = `
-        <h5>${key}</h5>
-        <div>
-            <div>
-                <h6>Product Code(s)</h6>
-            ${json.UDIToProductCodes.data.filter((match) => match.UDI_DI === udi.value).map((match) => match['Product Code']).join(', ')}
-            </div>
-            <div>
-                <h6>Country Selected</h6>
-                ${udiCountry.value}
-            </div>
-            <div>${value.map((link) => `<a href='${link}' target='blank'><button>${link.substring(link.lastIndexOf('/') + 1)}</button></a>`).join('')}</div>
-        </div>`;
-        block.querySelector('.ifu-result').append(entry);
-      });
-    }
-    block.querySelector('.ifu-result').classList.remove('no-result');
-  });
-}
-
-function populateProductCode(json, block) {
-  const productCode = block.querySelector('#product-code');
-  json.productCodes.data.forEach((value) => {
-    const option = document.createElement('option');
-    option.text = value.Code;
-    productCode.append(option);
-  });
-  productCode.disabled = false;
-
-  const productCodeCountry = block.querySelector('#product-code-country');
-  json.countries.data.forEach((country) => {
-    const option = document.createElement('option');
-    option.text = country['Country Name'];
-    option.value = country.ISO_3166_1_alpha_2_code;
-    productCodeCountry.append(option);
-  });
-  productCodeCountry.disabled = false;
-
-  const search = block.querySelector('#product-code-search');
-  search.disabled = false;
-  search.addEventListener('click', () => {
-    const assets = json.eIFUToProductCodes.data.filter((entry) => entry['Product Code'] === productCode.value)
-      .flatMap((entry) => json.IFUAssets.data.filter((asset) => asset.eIFU === entry.eIFU_ID
-        && asset.Country === productCodeCountry.value));
-    if (assets.length === 0) {
-      block.querySelector('.ifu-result').innerHTML = '<h4>Search results</h4><div><strong>No result</strong></div>';
-    } else {
-      block.querySelector('.ifu-result').innerHTML = '<h4>Search results</h4>';
-      const map = new Map();
-      assets.forEach((asset) => {
-        if (map.has(asset.Title)) {
-          const links = map.get(asset.Title);
-          links.push(asset.URL);
-        } else {
-          map.set(asset.Title, [asset.URL]);
-        }
-      });
-      map.forEach((value, key) => {
-        const entry = document.createElement('div');
-        entry.innerHTML = `
-        <h5>${key}</h5>
-        <div>
-            <div>
-                <h6>Product Code(s)</h6>
-            ${json.eIFUToProductCodes.data.filter((match) => match['Product Code'] === productCode.value).flatMap((match) => json.eIFUToProductCodes.data.filter((innerMatch) => innerMatch.eIFU_ID === match.eIFU_ID).map((innerMatch) => innerMatch['Product Code'])).join(', ')}
-            </div>
-            <div>
-                <h6>Country Selected</h6>
-                ${productCodeCountry.value}
-            </div>
-            <div>${value.map((link) => `<a href='${link}' target='blank'><button>${link.substring(link.lastIndexOf('/') + 1)}</button></a>`).join('')}</div>
-        </div>`;
-        block.querySelector('.ifu-result').append(entry);
-      });
-    }
-    block.querySelector('.ifu-result').classList.remove('no-result');
+    result.classList.remove('no-result');
   });
 }
 
@@ -231,50 +131,218 @@ async function populate(block) {
   const resp = await fetch('/en/dj-test.json?limit=10000');
   if (resp.ok) {
     const json = await resp.json();
-    populateIFU(json, block);
-    populateUDI(json, block);
-    populateProductCode(json, block);
+    const eIFUCode = block.querySelector('#eIFU-code');
+    const eIFUCountry = block.querySelector('#eIFU-country');
+    const eIFUSearch = block.querySelector('#eIFU-code-search');
+    const udiCode = block.querySelector('#udi-code');
+    const udiCountry = block.querySelector('#udi-country');
+    const udiSearch = block.querySelector('#udi-code-search');
+    const productCode = block.querySelector('#product-code');
+    const productCountry = block.querySelector('#product-country');
+    const productSearch = block.querySelector('#product-code-search');
+    const result = block.querySelector('.ifu-result');
+    populateSearch(
+      eIFUCode,
+      eIFUCountry,
+      eIFUSearch,
+      result,
+      () => json.eIFUs.data.map((value) => value.ID),
+      () => json.countries.data,
+      (idSelector, countrySelector) => json.IFUAssets.data.filter(
+        (asset) => asset.eIFU === idSelector.value && asset.Country === countrySelector.value,
+      ),
+      (idSelector) => json.eIFUToProductCodes.data
+        .filter((match) => match.eIFU_ID === idSelector.value)
+        .map((match) => match['Product Code']).join(', '),
+      [udiCode, udiCountry, productCode, productCountry],
+    );
+
+    populateSearch(
+      udiCode,
+      udiCountry,
+      udiSearch,
+      result,
+      () => json.UDIs.data.map((value) => value.Code),
+      () => json.countries.data,
+      (idSelector, countrySelector) => json.eIFUToUDIs.data
+        .filter((entry) => entry.UDI_DI === idSelector.value)
+        .flatMap((entry) => json.IFUAssets.data
+          .filter((asset) => asset.eIFU === entry.eIFU && asset.Country === countrySelector.value)),
+      (idSelector) => json.UDIToProductCodes.data
+        .filter((match) => match.UDI_DI === idSelector.value)
+        .map((match) => match['Product Code']).join(', '),
+      [eIFUCode, eIFUCountry, productCode, productCountry],
+    );
+
+    populateSearch(
+      productCode,
+      productCountry,
+      productSearch,
+      result,
+      () => json.productCodes.data.map((value) => value.Code),
+      () => json.countries.data,
+      (idSelector, countrySelector) => json.eIFUToProductCodes.data
+        .filter((entry) => entry['Product Code'] === idSelector.value)
+        .flatMap((entry) => json.IFUAssets.data
+          .filter((asset) => asset.eIFU === entry.eIFU_ID
+            && asset.Country === countrySelector.value)),
+      (idSelector) => json.eIFUToProductCodes.data
+        .filter((match) => match['Product Code'] === idSelector.value)
+        .flatMap((match) => json.eIFUToProductCodes.data
+          .filter((innerMatch) => innerMatch.eIFU_ID === match.eIFU_ID)
+          .map((innerMatch) => innerMatch['Product Code'])).join(', '),
+      [eIFUCode, eIFUCountry, udiCode, udiCountry],
+    );
   } else {
     throw new Error(`${resp.status}: ${resp.statusText}`);
   }
 }
 
 export default async function decorate(block) {
-  block.innerHTML = `
-    <div class="ifu-result no-result">
-        <h4>Search results</h4>
-    </div>
-    <div class="ifu">
-        <div class="ifu-selection">
-            <h5 class="ifu-title">Search by eIFU</h5>
-            <select id="eIFU-code" name="Select eIFU" disabled>
-                <option hidden disabled selected value>Select eIFU</option>
-            </select>
-            <select id="eIFU-country" name="Select Country" disabled>
-                <option hidden disabled selected value>Select Country</option>
-            </select>
-            <button id="eIFU-code-search"disabled>Search</button>
-        </div>
-        <div class="ifu-selection">
-            <h5 class="ifu-title">Search by UDI</h5>
-            <select id="udi-code" name="Select UDI" disabled>
-                <option hidden disabled selected value>Select UDI</option>
-            </select>
-            <select id="udi-country" name="Select Country" disabled>
-                <option hidden disabled selected value>Select Country</option>
-            </select>
-            <button id="udi-code-search" disabled>Search</button>
-        </div>
-        <div class="ifu-selection">
-            <h5 class="ifu-title">Search by Product Code</h5>
-            <select id="product-code" name="Select Product Code" disabled>
-                <option hidden disabled selected value>Select Product Code</option>
-            </select>
-            <select id="product-code-country" name="Select Country" disabled>
-                <option hidden disabled selected value>Select Country</option>
-            </select>
-            <button id="product-code-search" disabled>Search</button>
-        </div>
-    </div>`;
+  block.append(...buildTags([
+    {
+      name: 'div',
+      classes: ['ifu-result', 'no-result'],
+      next: [{ name: 'h4', text: await translate('ifuSearchTitle', 'Search results') }],
+    },
+    {
+      name: 'div',
+      class: 'ifu',
+      next: [
+        {
+          name: 'div',
+          class: 'ifu-selection',
+          next: [
+            {
+              name: 'h5',
+              class: 'ifu-title',
+              text: await translate('ifuSearchByIFU', 'Search by eIFU'),
+            },
+            {
+              name: 'select',
+              id: 'eIFU-code',
+              next: [
+                {
+                  name: 'option',
+                  text: await translate('ifuSelectIFU', 'Select eIFU'),
+                },
+              ],
+            },
+            {
+              name: 'select',
+              id: 'eIFU-country',
+              next: [
+                {
+                  name: 'option',
+                  text: await translate('ifuSelectCountry', 'Select Country'),
+                },
+              ],
+            },
+            {
+              name: 'button',
+              id: 'eIFU-code-search',
+              text: await translate('ifuSearch', 'Search'),
+            },
+          ],
+        },
+        {
+          name: 'div',
+          class: 'ifu-selection',
+          next: [
+            {
+              name: 'h5',
+              class: 'ifu-title',
+              text: await translate('ifuSearchByUDI', 'Search by UDI'),
+            },
+            {
+              name: 'select',
+              id: 'udi-code',
+              next: [
+                {
+                  name: 'option',
+                  text: await translate('ifuSelectUDI', 'Select UDI'),
+                },
+              ],
+            },
+            {
+              name: 'select',
+              id: 'udi-country',
+              next: [
+                {
+                  name: 'option',
+                  text: await translate('ifuSelectCountry', 'Select Country'),
+                },
+              ],
+            },
+            {
+              name: 'button',
+              id: 'udi-code-search',
+              text: await translate('ifuSearch', 'Search'),
+            },
+          ],
+        },
+        {
+          name: 'div',
+          class: 'ifu-selection',
+          next: [
+            {
+              name: 'h5',
+              class: 'ifu-title',
+              text: await translate('ifuSearchByProductCode', 'Search by Product Code'),
+            },
+            {
+              name: 'select',
+              id: 'product-code',
+              next: [
+                {
+                  name: 'option',
+                  text: await translate('ifuSelectProductCode', 'Select Product Code'),
+                },
+              ],
+            },
+            {
+              name: 'select',
+              id: 'product-country',
+              next: [
+                {
+                  name: 'option',
+                  text: await translate('ifuSelectCountry', 'Select Country'),
+                },
+              ],
+            },
+            {
+              name: 'button',
+              id: 'product-code-search',
+              text: await translate('ifuSearch', 'Search'),
+            },
+          ],
+        },
+      ],
+    },
+  ]));
+
+  function mix(name, title) {
+    let element = block.querySelector(`#${name}-code`);
+    element.name = `Select ${title}`;
+    element.disabled = true;
+    element.firstChild.hidden = true;
+    element.firstChild.disabled = true;
+    element.firstChild.selected = true;
+
+    element = block.querySelector(`#${name}-country`);
+    element.name = 'Select Country';
+    element.disabled = true;
+    element.firstChild.hidden = true;
+    element.firstChild.disabled = true;
+    element.firstChild.selected = true;
+
+    element = block.querySelector(`#${name}-code-search`);
+    element.disabled = true;
+  }
+
+  mix('eIFU', 'eIFU');
+  mix('udi', 'UDI');
+  mix('product', 'Product Code');
+
   populate(block);
 }
