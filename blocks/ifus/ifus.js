8ce7e31bd946a50d3a70b060c1c0224784617b7f
Karl Pauls <karlpauls@gmail.com>
2023-04-24 13:12:35 +0200

    Cleanup

--- blocks/ifus/ifus.js
+++ blocks/ifus/ifus.js
@@ -1,163 +1,144 @@
-import { translate } from '../../scripts/lib-franklin.js';
+import { createDomStructure, translate } from '../../scripts/lib-franklin.js';
 
-function createDomStructure(structure, parentElement = document.body) {
-  structure.forEach((element) => {
-    const domElement = document.createElement(element.type);
-    if (element.attributes) {
-      Object.keys(element.attributes).forEach((attr) => {
-        domElement.setAttribute(attr, element.attributes[attr]);
-      });
-    }
+async function handleSearch(selectors, allSelectors) {
+  selectors.result.innerHTML = '';
+  createDomStructure([{
+    type: 'h4',
+    textContent: await translate('ifuSearchTitle', 'Search results'),
+  }], selectors.result);
 
-    if (element.textContent) {
-      domElement.textContent = element.textContent;
-    }
+  allSelectors.filter((entry) => entry !== selectors)
+    .flatMap((entry) => [entry.code, entry.country])
+    .forEach((value) => { value.options[0].selected = true; });
 
-    if (element.children) {
-      createDomStructure(element.children, domElement);
-    }
+  selectors.result.classList.remove('no-result');
 
-    if (element.classes) {
-      element.classes.forEach((c) => domElement.classList.add(c));
-    }
+  const assets = selectors.code.value && selectors.country.value
+    ? selectors.assetFilter(selectors.code, selectors.country) : [];
 
-    if (element.position === 'prepend') {
-      parentElement.prepend(domElement);
+  if (assets.length === 0) {
+    createDomStructure([{
+      type: 'div',
+      children: [{ type: 'strong', textContent: await translate('ifuSearchNoResult', 'No result') }],
+    }], selectors.result);
+    return;
+  }
+
+  const map = new Map();
+  assets.forEach((asset) => {
+    if (map.has(asset.Title)) {
+      const links = map.get(asset.Title);
+      links.push(asset.URL);
     } else {
-      parentElement.appendChild(domElement);
+      map.set(asset.Title, [asset.URL]);
     }
   });
-}
 
-function populateSearch(
-  idSelector,
-  countrySelector,
-  searchButton,
-  result,
-  idProducer,
-  countryProducer,
-  assetFilter,
-  productCodes,
-  clear,
-) {
-  idProducer().forEach((value) => {
-    createDomStructure([{ type: 'option', textContent: value }], idSelector);
-  });
-  idSelector.disabled = false;
-
-  countryProducer().forEach((country) => {
+  await Promise.all(Array.from(map).map(async ([key, value]) => {
     createDomStructure([
-      { type: 'option', attributes: { value: country.ISO_3166_1_alpha_2_code }, textContent: country['Country Name'] },
-    ], countrySelector);
-  });
-  countrySelector.disabled = false;
-
-  searchButton.disabled = false;
-  searchButton.addEventListener('click', async () => {
-    clear.forEach((value) => { value.options[0].selected = true; });
-    const assets = idSelector.value && countrySelector.value
-      ? assetFilter(idSelector, countrySelector) : [];
-    result.innerHTML = '';
-    createDomStructure([{ type: 'h4', textContent: await translate('ifuSearchTitle', 'Search results') }], result);
-    if (assets.length === 0) {
-      createDomStructure([{ type: 'div', children: [{ type: 'strong', textContent: await translate('ifuSearchNoResult', 'No result') }] }], result);
-    } else {
-      const map = new Map();
-      assets.forEach((asset) => {
-        if (map.has(asset.Title)) {
-          const links = map.get(asset.Title);
-          links.push(asset.URL);
-        } else {
-          map.set(asset.Title, [asset.URL]);
-        }
-      });
-      await Promise.all(Array.from(map).map(async ([key, value]) => {
-        createDomStructure([
+      {
+        type: 'div',
+        children: [
+          { type: 'h5', textContent: key },
           {
             type: 'div',
             children: [
-              { type: 'h5', textContent: key },
               {
                 type: 'div',
                 children: [
                   {
                     type: 'div',
-                    children:
-                      [
-                        {
-                          type: 'div',
-                          textContent: productCodes(idSelector),
-                          children:
-                            [
-                              { type: 'h6', position: 'prepend', textContent: await translate('ifuSearchProductCodes', 'Product Code(s)') },
-                            ],
-                        },
-                      ],
-                  },
-                  {
-                    type: 'div',
-                    textContent: countrySelector.value,
-                    children:
-                      [
-                        { type: 'h6', position: 'prepend', textContent: await translate('ifuSearchCountrySelected', 'Country Selected') },
-                      ],
-                  },
-                  {
-                    type: 'div',
-                    children: value.map(
-                      (link) => ({
-                        type: 'a', attributes: { href: link, target: 'blank' }, children: [{ type: 'button', textContent: link.substring(link.lastIndexOf('/') + 1) }],
-                      }),
-                    ),
+                    textContent: selectors.productProducer(selectors.code),
+                    children: [{
+                      type: 'h6',
+                      position: 'prepend',
+                      textContent: await translate('ifuSearchProductCodes', 'Product Code(s)'),
+                    }],
                   },
                 ],
-              }],
+              },
+              {
+                type: 'div',
+                textContent: selectors.country.value,
+                children: [{
+                  type: 'h6',
+                  position: 'prepend',
+                  textContent: await translate('ifuSearchCountrySelected', 'Country Selected'),
+                }],
+              },
+              {
+                type: 'div',
+                children: value.map(
+                  (link) => ({
+                    type: 'a',
+                    attributes: { href: link, target: 'blank' },
+                    children: [{ type: 'button', textContent: link.substring(link.lastIndexOf('/') + 1) }],
+                  }),
+                ),
+              },
+            ],
           },
-        ], result);
-      }));
-    }
-    result.classList.remove('no-result');
-  });
+        ],
+      },
+    ], selectors.result);
+  }));
+}
+
+function populateSearch(selectors, allSelectors) {
+  const createOptions = (items, selector, text, attributes) => {
+    createDomStructure(
+      items.map((item) => ({ type: 'option', textContent: text?.(item) || item, attributes: attributes?.(item) })),
+      selector,
+    );
+    selector.disabled = false;
+  };
+
+  createOptions(selectors.idMap(), selectors.code);
+
+  createOptions(
+    selectors.countryMap(),
+    selectors.country,
+    (country) => country['Country Name'],
+    (country) => ({ value: country.ISO_3166_1_alpha_2_code }),
+  );
+
+  selectors.search.disabled = false;
+  selectors.search.addEventListener('click', async () => handleSearch(selectors, allSelectors));
 }
 
 async function populate(block) {
   const resp = await fetch('/en/dj-test.json?limit=10000');
-  if (resp.ok) {
-    const json = await resp.json();
-    const eIFUCode = block.querySelector('#eIFU-code');
-    const eIFUCountry = block.querySelector('#eIFU-country');
-    const eIFUSearch = block.querySelector('#eIFU-code-search');
-    const udiCode = block.querySelector('#udi-code');
-    const udiCountry = block.querySelector('#udi-country');
-    const udiSearch = block.querySelector('#udi-code-search');
-    const productCode = block.querySelector('#product-code');
-    const productCountry = block.querySelector('#product-country');
-    const productSearch = block.querySelector('#product-code-search');
-    const result = block.querySelector('.ifu-result');
+  if (!resp.ok) {
+    throw new Error(`${resp.status}: ${resp.statusText}`);
+  }
 
-    populateSearch(
-      eIFUCode,
-      eIFUCountry,
-      eIFUSearch,
-      result,
+  const json = await resp.json();
+
+  const getSelectors = (prefix, idMap, assetFilter, productProducer) => ({
+    code: block.querySelector(`#${prefix}-code`),
+    country: block.querySelector(`#${prefix}-country`),
+    search: block.querySelector(`#${prefix}-code-search`),
+    result: block.querySelector('.ifu-result'),
+    idMap,
+    countryMap: () => json.countries.data,
+    assetFilter,
+    productProducer,
+  });
+
+  const selectors = [
+    getSelectors(
+      'eIFU',
       () => json.eIFUs.data.map((value) => value.ID),
-      () => json.countries.data,
       (idSelector, countrySelector) => json.IFUAssets.data.filter(
         (asset) => asset.eIFU === idSelector.value && asset.Country === countrySelector.value,
       ),
       (idSelector) => json.eIFUToProductCodes.data
         .filter((match) => match.eIFU_ID === idSelector.value)
         .map((match) => match['Product Code']).join(', '),
-      [udiCode, udiCountry, productCode, productCountry],
-    );
-
-    populateSearch(
-      udiCode,
-      udiCountry,
-      udiSearch,
-      result,
+    ),
+    getSelectors(
+      'udi',
       () => json.UDIs.data.map((value) => value.Code),
-      () => json.countries.data,
       (idSelector, countrySelector) => json.eIFUToUDIs.data
         .filter((entry) => entry.UDI_DI === idSelector.value)
         .flatMap((entry) => json.IFUAssets.data
@@ -165,16 +146,10 @@ async function populate(block) {
       (idSelector) => json.UDIToProductCodes.data
         .filter((match) => match.UDI_DI === idSelector.value)
         .map((match) => match['Product Code']).join(', '),
-      [eIFUCode, eIFUCountry, productCode, productCountry],
-    );
-
-    populateSearch(
-      productCode,
-      productCountry,
-      productSearch,
-      result,
+    ),
+    getSelectors(
+      'product',
       () => json.productCodes.data.map((value) => value.Code),
-      () => json.countries.data,
       (idSelector, countrySelector) => json.eIFUToProductCodes.data
         .filter((entry) => entry['Product Code'] === idSelector.value)
         .flatMap((entry) => json.IFUAssets.data
@@ -185,11 +160,10 @@ async function populate(block) {
         .flatMap((match) => json.eIFUToProductCodes.data
           .filter((innerMatch) => innerMatch.eIFU_ID === match.eIFU_ID)
           .map((innerMatch) => innerMatch['Product Code'])).join(', '),
-      [eIFUCode, eIFUCountry, udiCode, udiCountry],
-    );
-  } else {
-    throw new Error(`${resp.status}: ${resp.statusText}`);
-  }
+    ),
+  ];
+
+  selectors.forEach((selector) => populateSearch(selector, selectors));
 }
 
 export default async function decorate(block) {
