e5e7c60c29983cd994ab0732de0db8aa12a3ae91
Acidham <acidham@gmail.com>
2023-05-10 08:03:19 +0200

    add Tab Block Navigation

--- blocks/tab-navigation/tab-navigation.js
+++ blocks/tab-navigation/tab-navigation.js
@@ -2,9 +2,9 @@ import { getMetadata } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   const links = block.querySelectorAll('a');
-  if (!links) return;
+  if (!links.length) return;
   const actualPage = getMetadata('og:url').split('/').slice(-1)[0].toLowerCase();
-  [...links].forEach((a) => {
+  links.forEach((a) => {
     const href = a.getAttribute('href').toLowerCase();
     if (href.includes(actualPage)) {
       a.classList.add('active');
