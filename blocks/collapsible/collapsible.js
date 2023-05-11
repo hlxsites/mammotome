2b6abe85ac4270d413825eb6cb4b567ed07f93d8
Miroslav Smiljanic <smiljani@adobe.com>
2023-05-11 16:40:24 +0200

    feat: added effect when doing toggle of the answer

--- blocks/collapsible/collapsible.js
+++ blocks/collapsible/collapsible.js
@@ -1,13 +1,13 @@
 export default function decorate(block) {
+  
   const processRow = (row) => {
-    row.classList.add('collapsed');
-
+    
     const rowContent = [...row.children];
     const questionArea = rowContent[0];
     const answerArea = rowContent[1];
 
     questionArea.className = 'question';
-    answerArea.className = 'answer';
+    answerArea.className = 'answer collapsed';
 
     const question = document.createElement('a');
 
@@ -17,16 +17,73 @@ export default function decorate(block) {
 
     questionArea.appendChild(question);
 
-    questionArea.addEventListener('click', () => {
-      row.classList.toggle('collapsed');
-      row.classList.toggle('expanded');
-    });
-
     const answerBlock = document.createElement('p');
     answerBlock.innerText = answerArea.innerText;
 
     answerArea.innerText = '';
     answerArea.appendChild(answerBlock);
+
+    const ontransitionend = () => {
+      answerArea.className = 'answer';
+      answerArea.removeEventListener('transitionend', ontransitionend);
+
+      answerArea.style.height = null;
+    }
+    
+    const collapseAnswer = () => {
+      const answerAreaHeight = answerArea.scrollHeight;
+
+
+      requestAnimationFrame(function() {
+        answerBlock.style.height = answerAreaHeight + 'px';
+        answerArea.className = 'answer transition';
+  
+        requestAnimationFrame(function() {
+          answerBlock.style.height = 0 + 'px';
+
+          answerArea.addEventListener('transitionend', function(){
+            answerArea.className = 'answer collapsed';
+          });
+          
+        });
+      });  
+
+      answerArea.setAttribute('data-expanded', 'false');
+    }  
+
+    const expandAnswer = () => {
+      answerArea.className = 'answer before-transition';
+      // answerArea.classList.toggle('before-transition');
+      // answerArea.classList.toggle('collapsed');
+      
+      
+      const answerAreaHeight = answerArea.scrollHeight;
+
+      answerArea.addEventListener('transitionend', ontransitionend);
+
+      answerArea.className = 'answer before-transition transition';
+
+      answerBlock.style.height = answerAreaHeight + 'px';
+      // answerArea.style.height = answerAreaHeight + 'px';
+      
+      answerArea.setAttribute('data-expanded', 'true');
+    } 
+
+    const toggleAnswer = () => {
+
+      const isExpanded = answerArea.getAttribute('data-expanded') === 'true';
+
+      if(isExpanded) {
+        collapseAnswer();
+      } else {
+        expandAnswer();
+      }
+
+      // answerArea.classList.toggle('collapsed');
+      // answerArea.classList.toggle('expanded');
+    }
+
+    questionArea.addEventListener('click', toggleAnswer);
   };
 
   [...block.children].forEach(processRow);
