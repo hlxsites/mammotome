72aa2a43854b9f012d6478b0beb72d7669e0e48c
Miroslav Smiljanic <smiljani@adobe.com>
2023-05-17 10:19:00 +0200

    Revert "fix: user classList.add and classList.toggle"
    This reverts commit 4a6553c7edd048fd6dc01bddbfdf8a024a706061.

--- blocks/collapsible/collapsible.js
+++ blocks/collapsible/collapsible.js
@@ -1,9 +1,11 @@
 export default function decorate(block) {
   const processRow = (row) => {
-    const [questionArea, answerArea] = Array.from(row.children);
+    const rowContent = [...row.children];
+    const questionArea = rowContent[0];
+    const answerArea = rowContent[1];
 
-    questionArea.classList.add('question');
-    answerArea.classList.add('answer', 'collapsed');
+    questionArea.className = 'question';
+    answerArea.className = 'answer collapsed';
 
     const question = document.createElement('a');
 
@@ -18,15 +20,14 @@ export default function decorate(block) {
     answerArea.appendChild(answerBlock);
 
     const onTransitionendExpand = () => {
-      answerArea.classList.toggle('transition');
+      answerArea.className = 'answer';
       answerArea.removeEventListener('transitionend', onTransitionendExpand);
       answerArea.style = null;
     };
 
-    const onTransitionendCollapse = () => {
-      answerArea.classList.toggle('transition');
-      answerArea.classList.toggle('collapsed');
-      answerArea.removeEventListener('transitionend', onTransitionendCollapse);
+    const onTransitionendCollpse = () => {
+      answerArea.className = 'answer collapsed';
+      answerArea.removeEventListener('transitionend', onTransitionendCollpse);
       answerArea.style = null;
     };
 
@@ -35,41 +36,48 @@ export default function decorate(block) {
 
       requestAnimationFrame(() => {
         answerArea.style.height = answerAreaHeight;
-        answerArea.classList.add('transition');
+        answerArea.className = 'answer transition';
 
         requestAnimationFrame(() => {
           answerArea.style.height = '0px';
           answerArea.style.paddingTop = '0px';
           answerArea.style.paddingBottom = '0px';
 
-          answerArea.addEventListener('transitionend', onTransitionendCollapse);
+          answerArea.addEventListener('transitionend', onTransitionendCollpse);
         });
       });
+
+      answerArea.setAttribute('data-expanded', 'false');
+
       questionArea.classList.toggle('expanded');
     };
 
     const expandAnswer = () => {
-      answerArea.classList.replace('collapsed', 'before-transition');
+      answerArea.className = 'answer before-transition';
 
       const answerAreaHeight = answerArea.scrollHeight;
 
       answerArea.addEventListener('transitionend', onTransitionendExpand);
 
       answerArea.style.height = `${answerAreaHeight}px`;
-      answerArea.classList.replace('before-transition', 'transition');
+      answerArea.className = 'answer transition';
+
+      answerArea.setAttribute('data-expanded', 'true');
+
       questionArea.classList.toggle('expanded');
     };
 
     const toggleAnswer = () => {
-      if (answerArea.classList.contains('collapsed')) {
-        expandAnswer();
-      } else {
+      const isExpanded = answerArea.getAttribute('data-expanded') === 'true';
+
+      if (isExpanded) {
         collapseAnswer();
+      } else {
+        expandAnswer();
       }
     };
-
     questionArea.addEventListener('click', toggleAnswer);
   };
 
-  Array.from(block.children).forEach(processRow);
+  [...block.children].forEach(processRow);
 }
