c704ffeb44ef3e0a01dc99d8cda99bfcbbbc8a27
Miroslav Smiljanic <smmiroslav@gmail.com>
2023-05-19 09:41:23 +0200

    feat: collapsible (#125)
    * feat: added js and css files
    
    * feat: questions and collpsible answers
    
    * feat: added effect when doing toggle of the answer
    
    * feat: first working version of transitioning effect
    
    * feat: color for expanded question
    
    * feat: added triangle in front of the question
    
    * fix: css lint
    
    * fix: code formatting
    
    * fix: lint js
    
    * fix: user classList.add and classList.toggle
    
    * fix: cursor as pointer
    
    * Revert "fix: user classList.add and classList.toggle"
    
    This reverts commit 4a6553c7edd048fd6dc01bddbfdf8a024a706061.
    
    * Revert "Revert "fix: user classList.add and classList.toggle""
    
    This reverts commit 72aa2a43854b9f012d6478b0beb72d7669e0e48c.
    
    * feat: use 0 instad opx
    
    * fix: use height:0 instead of dispaly:none for answer in initial state
    
    * fix: paragraph for the answer
    
    ---------
    
    Co-authored-by: Miroslav Smiljanic <smiljani@adobe.com>

---
+++ blocks/collapsible/collapsible.js
@@ -0,0 +1,38 @@
+export default function decorate(block) {
+  const processRow = (row) => {
+    const [questionArea, answerArea] = Array.from(row.children);
+
+    questionArea.classList.add('question');
+    answerArea.classList.add('answer');
+
+    const question = document.createElement('a');
+
+    const answerBlock = document.createElement('p');
+    answerBlock.innerText = answerArea.innerText;
+
+    answerArea.innerText = '';
+    answerArea.appendChild(answerBlock);
+
+    question.innerText = questionArea.innerText;
+    questionArea.innerText = '';
+    questionArea.appendChild(question);
+
+    answerArea.classList.add('collapsed');
+
+    questionArea.addEventListener('click', () => {
+      questionArea.classList.toggle('expanded');
+
+      if (questionArea.classList.contains('expanded')) {
+        // Expand the answer
+        answerArea.style.height = `${answerArea.scrollHeight}px`;
+      } else {
+        // Collapse the answer
+        answerArea.style.height = '0';
+      }
+
+      answerArea.classList.toggle('collapsed');
+    });
+  };
+
+  Array.from(block.children).forEach(processRow);
+}
