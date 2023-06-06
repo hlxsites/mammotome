3165663ded5cd86d69f07401f0d80c46215db240
Damian Zehnder <zehnder@adobe.com>
2023-06-06 11:09:59 +0200

    fix: validate url and improve lhs

--- blocks/soundcloud/soundcloud.js
+++ blocks/soundcloud/soundcloud.js
@@ -1,14 +1,41 @@
+import addIframes from '../../scripts/delayed.js';
+
+const soundcloud = {
+  origin: 'https://w.soundcloud.com',
+  url: 'https://api.soundcloud.com/tracks/',
+};
+
+const calculateIframeHeight = (text) => {
+  // Extract the height value from the iframe HTML
+  const heightRegex = /height="(\d+)"/;
+  const heightMatch = text.match(heightRegex);
+  if (heightMatch && heightMatch[1]) {
+    return parseInt(heightMatch[1], 10);
+  }
+  return 0;
+};
 
 export default async function decorate(block) {
-  block.querySelectorAll('.soundcloud > div').forEach((el) => {
-    el.classList.add('soundcloud-col');
-    const iframeParent = el.children[1];
-    const text = iframeParent.textContent;
-    const regex = /<iframe.*?<\/iframe>/;
-    const match = text.match(regex);
-    if (match && match[0]) {
-      const iframeTag = match[0];
-      iframeParent.innerHTML = iframeTag;
+  const iframes = Array.from(block.querySelectorAll('.soundcloud > div'))
+    .map((el) => ({
+      element: el,
+      iframe: el.children[1],
+      text: el.children[1].textContent,
+    }))
+    .filter(({ text }) => /<iframe.*?<\/iframe>/.test(text));
+
+  iframes.forEach(({ element, text }) => {
+    const match = text.match(/src="(.*?)"/);
+    if (match && match[1]) {
+      const url = new URL(decodeURIComponent(match[1]));
+      const urlParam = new URLSearchParams(url.search).get('url');
+      if (url.origin === soundcloud.origin && urlParam?.startsWith(soundcloud.url)) {
+        const height = calculateIframeHeight(text);
+        element.children[1].style.height = `${height}px`;
+        element.classList.add('soundcloud-col');
+      }
     }
   });
+
+  addIframes(iframes);
 }
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -12,3 +12,9 @@ try {
 } catch (error) { /* empty */ }
 
 document.dispatchEvent(new Event('franklin.delayed_completed'));
+
+export default function addIframes(iframes) {
+  iframes.forEach(({ element, text }) => {
+    element.children[1].innerHTML = text;
+  });
+}
