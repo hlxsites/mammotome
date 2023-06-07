e72274e0315ec134c33377519b3cd4e633d9b468
Karl Pauls <karlpauls@gmail.com>
2023-06-07 11:47:17 +0200

    Fix asset url adjustment duplication

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -395,6 +395,18 @@ function productDBMatches(entry, country, language) {
   return true;
 }
 
+export function adjustAssetURL(asset) {
+  if (asset?.URL) {
+    const url = new URL(asset.URL, window.location);
+    if (url.hostname.endsWith('-mammotome--hlxsites.hlx.page')
+      || url.hostname.endsWith('-mammotome--hlxsites.hlx.live')
+      || url.hostname === 'localhost') {
+      asset.URL = url.pathname;
+    }
+  }
+  return asset;
+}
+
 export async function getProduct(page, country, language) {
   const productDB = await getProductDB();
 
@@ -411,17 +423,7 @@ export async function getProduct(page, country, language) {
 
     product.assets = productDB.ProductAsset.data.filter(
       (asset) => asset.Page === product.Page && productDBMatches(asset, country, language),
-    ).map((asset) => {
-      if (asset.URL) {
-        const url = new URL(asset.URL, window.location);
-        if (url.hostname.endsWith('-mammotome--hlxsites.hlx.page')
-          || url.hostname.endsWith('-mammotome--hlxsites.hlx.live')
-          || url.hostname === 'localhost') {
-          asset.URL = url.pathname;
-        }
-      }
-      return asset;
-    });
+    ).map(adjustAssetURL);
   }
 
   return product;
