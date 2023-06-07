f94414f46be91e67e4eb28740ad7b9ba4ef3dada
Damian Zehnder <zehnder@adobe.com>
2023-06-07 16:54:39 +0200

    feat: optimize performance and include title div from shared html

--- blocks/soundcloud/soundcloud.js
+++ blocks/soundcloud/soundcloud.js
@@ -1,46 +1,80 @@
 const SOUNDCLOUD = {
-  origin: 'https://w.soundcloud.com',
-  url: 'https://api.soundcloud.com/tracks/',
+  origin: 'https://soundcloud.com',
+  iframeOrigin: 'https://w.soundcloud.com',
+  apiUrl: 'https://api.soundcloud.com/tracks/',
 };
 
 const calculateIframeHeight = (text) => {
   // Extract the height value from the iframe HTML
   const heightRegex = /height="(\d+)"/;
   const heightMatch = text.match(heightRegex);
-  if (heightMatch && heightMatch[1]) {
-    return parseInt(heightMatch[1], 10);
-  }
-  return 0;
+  return heightMatch && heightMatch[1] ? parseInt(heightMatch[1], 10) : 0;
 };
 
 export default async function decorate(block) {
-  const iframes = Array.from(block.querySelectorAll('.soundcloud > div'))
-    .map((el) => ({
-      element: el,
-      iframe: el.children[1],
-      text: el.children[1].textContent,
-    }))
-    .filter(({ text }) => /<iframe.*?<\/iframe>/.test(text));
+  const elements = Array.from(block.querySelectorAll('.soundcloud > div'));
+  elements.reduce((podcasts, element) => {
+    const textContent = element.children[1]?.textContent ?? '';
+    const iframe = /<iframe.*?<\/iframe>/.exec(textContent)?.[0] ?? '';
+    const div = /<div.*?<\/div>/.exec(textContent)?.[0] ?? '';
 
-  iframes.forEach(({ element, text }) => {
-    const match = text.match(/src="(.*?)"/);
-    if (match && match[1]) {
-      const url = new URL(decodeURIComponent(match[1]));
+    const src = iframe.match(/src="(.*?)"/);
+    if (src && src[1]) {
+      const url = new URL(decodeURIComponent(src[1]));
       const urlParam = new URLSearchParams(url.search).get('url');
-      if (url.origin === SOUNDCLOUD.origin && urlParam?.startsWith(SOUNDCLOUD.url)) {
-        const height = calculateIframeHeight(text);
-        const soundCloudFrame = document.createElement('iframe');
-        soundCloudFrame.height = `${height}px`;
-        soundCloudFrame.width = '100%';
-        soundCloudFrame.setAttribute('loading', 'lazy');
-        soundCloudFrame.setAttribute('scrolling', 'no');
-        soundCloudFrame.setAttribute('frameborder', 'no');
-        soundCloudFrame.setAttribute('allow', 'autoplay');
-        soundCloudFrame.src = url.toString();
-        element.children[1].innerHTML = '';
-        element.children[1].appendChild(soundCloudFrame);
-        element.classList.add('soundcloud-col');
+      if (url.origin === SOUNDCLOUD.iframeOrigin && urlParam?.startsWith(SOUNDCLOUD.apiUrl)) {
+        podcasts.push({
+          element, iframe, url, div,
+        });
       }
+    }
+    return podcasts;
+  }, []).forEach(({
+    element, iframe, url, div,
+  }) => {
+    const height = calculateIframeHeight(iframe);
+    const soundCloudFrame = document.createElement('iframe');
+    soundCloudFrame.height = `${height}px`;
+    soundCloudFrame.width = '100%';
+    soundCloudFrame.setAttribute('loading', 'lazy');
+    soundCloudFrame.setAttribute('scrolling', 'no');
+    soundCloudFrame.setAttribute('frameborder', 'no');
+    soundCloudFrame.setAttribute('allow', 'autoplay');
+    soundCloudFrame.src = url.toString();
+    element.children[1].innerHTML = '';
+    element.children[1].appendChild(soundCloudFrame);
+    element.classList.add('soundcloud-col');
+
+    if (div.length > 0) {
+      const divElement = document.createElement('div');
+      divElement.innerHTML = div;
+
+      const links = divElement.getElementsByTagName('a');
+      const channel = links[0];
+      const podcast = links[1];
+
+      const titleDiv = document.createElement('div');
+      titleDiv.classList.add('soundcloud-title');
+
+      const channelElement = document.createElement('a');
+      channelElement.href = new URL(channel.href).origin === SOUNDCLOUD.origin ? channel.href : '';
+      channelElement.title = channel.title;
+      channelElement.target = channel.target;
+      channelElement.textContent = channel.textContent;
+
+      const separator = document.createTextNode(' · ');
+
+      const podcastElement = document.createElement('a');
+      podcastElement.href = podcast.href;
+      podcastElement.title = podcast.title;
+      podcastElement.target = podcast.target;
+      podcastElement.textContent = podcast.textContent;
+
+      titleDiv.appendChild(channelElement);
+      titleDiv.appendChild(separator);
+      titleDiv.appendChild(podcastElement);
+
+      element.children[1].appendChild(titleDiv);
     }
   });
 }
