36280a7f12c8c2881d96c366518814cdca31e28e
Karl Pauls <karlpauls@gmail.com>
2023-06-12 19:58:39 +0200

    Await lazy

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -249,9 +249,7 @@ function integrateMartech(parent, id) {
  */
 async function loadLazy(doc) {
   const main = doc.querySelector('main');
-  await new Promise((resolve) => {
-    loadCSS('https://use.typekit.net/tni6afe.css', resolve);
-  });
+  loadCSS('https://use.typekit.net/tni6afe.css', null);
   await loadBlocks(main);
 
   const { hash } = window.location;
