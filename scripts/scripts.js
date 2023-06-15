d22340f125a20a030dda7fa5822ad38935b7ec13
Karl Pauls <karlpauls@gmail.com>
2023-06-15 10:05:24 +0200

    Use fonts from adobe font server

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -259,6 +259,8 @@ async function loadLazy(doc) {
   const main = doc.querySelector('main');
   await loadBlocks(main);
 
+  loadCSS('https://use.typekit.net/tni6afe.css', null);
+
   const { hash } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
