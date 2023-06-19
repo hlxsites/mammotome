9950f0b4de7d782ed25d40e435c6f8a51db158ed
Karl Pauls <karlpauls@gmail.com>
2023-06-20 01:29:21 +0200

    Rework to cleanup, take section styles into account properly

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -38,38 +38,27 @@ const createMetadata = (main, document) => {
   return meta;
 };
 
-const createSectionMetadata = (text, document) => {
+const createSectionMetadata = (top, text, img, document) => {
   const table = [['Section Metadata']];
-  const style = ['Style'];
-  style.push(text);
-  table.push(style);
-  return WebImporter.DOMUtils.createTable(table, document);
-};
-
-const addSectionDividers = (section, document) => {
-  const br = document.createElement('div');
-  br.textContent = '---';
-  section.parentElement.insertBefore(br, section);
-  const br2 = document.createElement('div');
-  br2.textContent = '---';
-  section.append(br2);
-};
-
-function topLevel(section) {
-  if (section.parentElement) {
-    if (section.parentElement.tagName.toLowerCase() !== 'section') {
-      return topLevel(section.parentElement);
-    }
-    return false;
+  if (text.length > 0) {
+    const style = ['Style'];
+    style.push(text);
+    table.push(style);
   }
-  return true;
-}
+  if (img) {
+    table.push(['background-image', img]);
+  }
+  if (table.length > 1) {
+    top.append(WebImporter.DOMUtils.createTable(table, document));
+  }
+};
 
 function getProductReference(section) {
-  return Array.from(section.querySelectorAll('a')).filter((a) => a.href.match(/\/product-support\/(.+)/));
+  return Array.from(section.querySelectorAll('a')).filter((a) => a.href.match(/\/product-support\/(.+)/) || a.href.match(/\/documentazione-sui-prodotti\/(.+)/));
 }
 
 export default {
+  REQUIRED_STYLES: ['display', 'background-image', 'background-color', 'color', 'font-size', 'font-weight', 'text-transform', 'letter-spacing', 'opacity'],
   /**
    * Apply DOM operations to the provided document and return
    * the root element to be then transformed to Markdown.
@@ -98,121 +87,164 @@ export default {
       '#onetrust-consent-sdk',
     ]);
 
+    let topSections = Array.from(main.querySelectorAll('.elementor-top-section'));
+    topSections.filter((top) => top.style.display === 'none').forEach((top) => top.remove());
+    topSections = Array.from(main.querySelectorAll('.elementor-top-section'));
+    let sectionCount = 0;
+    topSections.forEach((top) => {
+      console.log(top);
+      // Massage hero
+      if (sectionCount === 0) {
+        if (top.classList.contains('elementor-section-full_width')) {
+          const img = WebImporter.DOMUtils.getImgFromBackground(top, document);
+          if (img) {
+            top.prepend(img);
+          }
+          Array.from(top.querySelectorAll('img')).filter((i) => i.src.match(/.*\/Hero-curve.svg/)).forEach((i) => {
+            i.remove();
+          });
+        }
+        if (getProductReference(top).length > 0) {
+          const as = getProductReference(top);
+          let outer = as[0].parentElement;
+          while (!Array.from(outer.classList).includes('elementor-row')) {
+            outer = outer.parentElement;
+          }
+          const table = [];
+          table[0] = ['Product Reference'];
+          table[1] = [as.find((a) => a.href.match(/\/product-support\/(.+)/))?.href.match(/\/product-support\/([^/]*)(\/)?/)[1]];
+          outer.replaceWith(WebImporter.DOMUtils.createTable(table, document));
+        }
+      } else if (top.classList.contains('elementor-section-content-middle')) {
+        let header = 'Columns';
+        if (Array.from(top.querySelectorAll('.elementor-column-wrap').values()).some((wrap) => wrap.style.backgroundImage.match(/url\(.*\/Rectangle-BG-2.svg\)/))) {
+          header += ' (image color light blue)';
+        }
+        const table = [[header]];
+        table.push(Array.from(top.querySelectorAll('.elementor-column').values()));
+        if (table.length > 1) {
+          top.innerHTML = '';
+          top.append(WebImporter.DOMUtils.createTable(table, document));
+        }
+      }
+
+      const boxed = Array.from(top.querySelectorAll('.elementor-widget-image-box').values());
+      boxed.push(...Array.from(top.querySelectorAll('article').values()));
+
+      if (boxed.length > 0) {
+        const div = document.createElement('div');
+        boxed[0].replaceWith(div);
+
+        const table = [['Cards']];
+
+        boxed.forEach((m) => {
+          m.remove();
+          const tr = [];
+          if (m.tagName.toLowerCase() === 'article') {
+            const img = m.querySelector('.elementor-post__thumbnail img');
+            img.parentElement.parentElement.remove();
+            tr.push(img);
+            tr.push(m);
+          } else {
+            const img = m.querySelector('.elementor-image-box-img  img');
+            img.parentElement.parentElement.remove();
+            tr.push(img);
+            tr.push(m);
+          }
+          table.push(tr);
+        });
+        div.replaceWith(WebImporter.DOMUtils.createTable(table, document));
+      }
+
+      let sectionStyle = '';
+      let sectionStyleDivider = '';
+      let backgroundImage;
+
+      if (top.style['background-image'] && top.style['background-image'].match(/url\(.*\/Mammotome-BG_Pattern-1.svg\)/)) {
+        sectionStyle += sectionStyleDivider + 'Logo primary background';
+        sectionStyleDivider = ', ';
+      }
+      if (Array.from(top.querySelectorAll('img')).some((img) => img.src.match(/.*\/Hero-curve-flipped.svg/))) {
+        Array.from(top.querySelectorAll('img')).forEach((img) => img.remove());
+        sectionStyle += sectionStyleDivider + 'arc top';
+        sectionStyleDivider = ', ';
+      }
+      let skipBackground = false;
+      if (top.querySelector('.elementor-background-overlay')) {
+        const el = top.querySelector('.elementor-background-overlay');
+        if (el.style.backgroundColor === 'rgb(72, 126, 217)') {
+          sectionStyle += sectionStyleDivider + 'accent secondary blur overlay';
+          sectionStyleDivider = ', ';
+        } else if (el.style.backgroundColor === 'rgb(255, 255, 255)') {
+          skipBackground = true;
+        } else if (el.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
+          sectionStyle += sectionStyleDivider + 'gradient secondary blur overlay';
+          sectionStyleDivider = ', ';
+        }
+      }
+
+      if (top.querySelector('.elementor-motion-effects-layer')) {
+        const el = top.querySelector('.elementor-motion-effects-layer');
+        if (!skipBackground) {
+          const img = WebImporter.DOMUtils.replaceBackgroundByImg(el, document);
+          if (img !== el) {
+            if (img.src.match(/.*\/Mammotome-M_Icon-Black.svg/)) {
+              img.remove();
+            } else {
+              backgroundImage = img;
+            }
+          }
+        }
+        if (el.style.backgroundImage === 'rgb(72, 126, 217)') {
+          sectionStyle += sectionStyleDivider + 'Base secondary blur overlay';
+          sectionStyleDivider = ', ';
+        }
+      }
+
+      if (top.style.backgroundColor === 'rgb(95, 141, 218)') {
+        sectionStyle += sectionStyleDivider + 'Base secondary solid background';
+        sectionStyleDivider = ', ';
+      }
+
+      if (top.querySelector('.elementor-heading-title')) {
+        const el = top.querySelector('.elementor-heading-title');
+        if (el.style.textTransform === 'uppercase') {
+          sectionStyle += sectionStyleDivider + 'header-uppercase';
+          sectionStyleDivider = ', ';
+        }
+        if (el.style.letterSpacing === '4px') {
+          sectionStyle += sectionStyleDivider + 'text-wide';
+          sectionStyleDivider = ', ';
+        }
+        if (el.style.fontSize === '28.799999px') {
+          sectionStyle += sectionStyleDivider + 'text-large';
+          sectionStyleDivider = ', ';
+        }
+        if (el.style.color === 'rgb(255, 255, 255)') {
+          sectionStyle += sectionStyleDivider + 'inverted text';
+          sectionStyleDivider = ', ';
+        }
+        if (el.style.color === 'rgb(132, 50, 155)') {
+          sectionStyle += sectionStyleDivider + 'header-colored';
+          sectionStyleDivider = ', ';
+        }
+      } else if (top.querySelector('p')) {
+        if (top.querySelector('p').style.color === 'rgb(255, 255, 255)') {
+          sectionStyle += sectionStyleDivider + 'inverted text';
+          sectionStyleDivider = ', ';
+        }
+      }
+
+      createSectionMetadata(top, sectionStyle, backgroundImage, document);
+
+      top.parentElement.insertBefore(document.createElement('hr'), top.nextSibling);
+
+      sectionCount += 1;
+    });
+
     // create the metadata block and append it to the main element
     createMetadata(main, document);
 
-    const sectionElements = main.querySelectorAll('.elementor-section');
-
-    if (sectionElements?.length > 0) {
-      let sections = Array.from(sectionElements.values());
-      if (sections[0].classList.contains('elementor-section-full_width')) {
-        const img = WebImporter.DOMUtils.getImgFromBackground(sections[0], document);
-        if (img) {
-          sections[0].prepend(img);
-        }
-        Array.from(sections[0].querySelectorAll('img')).filter((i) => i.src.match(/.*\/Hero-curve.svg/)).forEach((i) => {
-          i.remove();
-        });
-        sections = sections.slice(1);
-      } else if (getProductReference(sections[0]).length > 0) {
-        const as = getProductReference(sections[0]);
-        let outer = as[0].parentElement;
-        while (!Array.from(outer.classList).includes('elementor-row')) {
-          outer = outer.parentElement;
-        }
-        const table = [];
-        table[0] = ['Product Reference'];
-        table[1] = [as.find((a) => a.href.match(/\/product-support\/(.+)/))?.href.match(/\/product-support\/([^/]*)(\/)?/)[1]];
-        outer.replaceWith(WebImporter.DOMUtils.createTable(table, document));
-      }
-      let middles = [];
-      let boxed = [];
-      sections.forEach((section) => {
-        if (section.classList.contains('elementor-section-content-middle')) {
-          middles.push(section);
-        } else {
-          if (middles.length > 1) {
-            const parent = document.createElement('section');
-            middles[0].replaceWith(parent);
-            const table = [['Columns (image color light blue)']];
-            let tr = 1;
-            middles.forEach((m) => {
-              m.remove();
-              const cc = [];
-              Array.from(m.querySelectorAll('.elementor-column').values()).forEach((c) => {
-                cc.push(c);
-              });
-              table[tr] = cc;
-              tr += 1;
-            });
-            parent.append(WebImporter.DOMUtils.createTable(table, document));
-          }
-          middles = [];
-          if (!section.querySelector('section') && (section.querySelector('.elementor-widget-image-box'))) {
-            boxed.push(section);
-          } else if (!section.querySelector('section') && section.querySelector('article')) {
-            Array.from(section.querySelectorAll('article')).forEach((a) => boxed.push(a));
-          } else {
-            if (boxed.length > 0) {
-              const parent = document.createElement('section');
-              let parentSection = boxed[0];
-              while (parentSection.tagName.toLowerCase() !== 'section' || !topLevel(parentSection)) {
-                parentSection = parentSection.parentElement;
-              }
-              parentSection.parentElement.insertBefore(parent, parentSection.nextSibling);
-              const table = [['Cards']];
-
-              boxed.forEach((m) => {
-                m.remove();
-                const tr = [];
-                if (m.tagName.toLowerCase() === 'article') {
-                  const img = m.querySelector('.elementor-post__thumbnail img');
-                  img.parentElement.parentElement.remove();
-                  tr.push(img);
-                  tr.push(m);
-                } else {
-                  const img = m.querySelector('.elementor-image-box-img  img');
-                  img.parentElement.parentElement.remove();
-                  tr.push(img);
-                  tr.push(m);
-                }
-                table.push(tr);
-              });
-              parent.append(WebImporter.DOMUtils.createTable(table, document));
-              boxed = [];
-            }
-            if (section.style['background-image'] && section.style['background-image'].match(/url\(.*\/Mammotome-BG_Pattern-1.svg\)/)) {
-              let text = 'Logo primary background, inverted text';
-              if (Array.from(section.querySelectorAll('img')).some((img) => img.src.match(/.*\/Hero-curve-flipped.svg/))) {
-                text += ', arc top';
-              }
-              const table = createSectionMetadata(text, document);
-              section.append(table);
-              addSectionDividers(section, document);
-            }
-          }
-        }
-        Array.from(section.querySelectorAll('img')).filter((img) => img.src.match(/.*\/Hero-curve-flipped.svg/)).forEach((img) => {
-          img.remove();
-        });
-      });
-    }
-    const sectionsE = main.querySelectorAll('section');
-
-    if (sectionsE?.length > 0) {
-      let sections = Array.from(sectionsE.values());
-      if (sections[0].classList.contains('elementor-section-full_width')) {
-        sections = sections.slice(1);
-      }
-      sections.forEach((section) => {
-        if (!section.querySelector('table') && section.querySelector('h2.elementor-heading-title')) {
-          const text = 'Header-wide, header-uppercase, header-colored';
-          const table = createSectionMetadata(text, document);
-          section.append(table);
-          addSectionDividers(section, document);
-        }
-      });
-    }
     return main;
   },
 
