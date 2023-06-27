8a4faf99dfa7a42ad40fd56cd9ed72c04c3990d4
Damian Zehnder <zehnder@adobe.com>
2023-06-27 14:46:37 +0200

    fix: transform not working properly in Safari

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -5,7 +5,7 @@ let removeVideo;
 let escHandler;
 
 const CSS_CLASS_NAME_ICON_PLAY_VIDEO = 'icon-playvideo';
-const HTML_PLAY_ICON = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16px" height="16px">\n'
+const HTML_PLAY_ICON = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="99.2px" height="99.2px">\n'
   + '    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>\n'
   + '    <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>\n'
   + '</svg>';
