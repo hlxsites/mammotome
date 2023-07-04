e37a20f48b9156528d8b53ede9b24bebe0bb3fac
Pareesh Gupta <pareesh@Pareeshs-MacBook-Pro.local>
2023-07-04 14:57:57 +0530

    feat(form): wizard implementation

--- blocks/form/wizard.js
+++ blocks/form/wizard.js
@@ -2,7 +2,7 @@ function createButton(fd) {
   const button = document.createElement('button');
   button.type = 'button';
   button.textContent = fd.Label;
-  button.className = 'form-wizard-step-button';
+  button.className = 'form-wizard-button';
   return button;
 }
 
@@ -34,17 +34,15 @@ function moveToPrev(current) {
 
 export default async function decorate(form) {
   form.classList.add('form-layout-wizard');
-  [...form.children].forEach((step) => {
-    const current = form.getElementsByClassName('current-wizard-step');
-    const wrapper = document.createElement('div');
-    wrapper.className = 'form-wizard-step-button-wrapper';
-    const prev = createButton({ Label: 'BACK' });
-    prev.onclick = () => moveToPrev(current[0]);
-    const next = createButton({ Label: 'NEXT' });
-    next.onclick = () => moveToNext(current[0]);
-    const submit = step.querySelector('.form-submit-wrapper');
-    wrapper.append(...(submit ? [prev, next, submit] : [prev, next]));
-    step.append(wrapper);
-  });
+  const wrapper = document.createElement('div');
+  wrapper.className = 'form-wizard-button-wrapper';
+  const current = form.getElementsByClassName('current-wizard-step');
+  const prev = createButton({ Label: 'BACK' });
+  prev.onclick = () => moveToPrev(current[0]);
+  const next = createButton({ Label: 'NEXT' });
+  next.onclick = () => moveToNext(current[0]);
+  const submit = form.querySelector('.form-submit-wrapper');
+  wrapper.append(prev, next, submit);
+  form.append(wrapper);
   form.children[0]?.classList.add('current-wizard-step');
 }
