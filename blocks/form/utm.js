30928fe16a8d020e705e55a8c0098dff4ace76e7
Pareesh Gupta <pareesh@pareeshs-mbp.corp.adobe.com>
2023-07-06 18:41:45 +0530

    Hidden fields for forms

--- blocks/form/utm.js
+++ blocks/form/utm.js
@@ -1,17 +1,17 @@
-const allowedUTMs = [
-  'utm_campaign',
-  'utm_source',
-  'utm_medium',
-  'utm_content',
-  'utm_term',
-  'gclid',
-];
+const fieldNameParamUTMMap = {
+  UTM_Campaign__c: 'utm_campaign',
+  UTM_Source__c: 'utm_source',
+  UTM_Medium__c: 'utm_medium',
+  UTM_Content__c: 'utm_content',
+  UTM_Term__c: 'utm_term',
+  GCLID__c: 'gclid',
+};
 
 export default async function decorate(form) {
   const queryParams = new URLSearchParams(window.location.search);
-  allowedUTMs.forEach((utm) => {
-    const values = queryParams.getAll(utm);
-    const input = form.querySelector(`input[type=hidden][name=${utm}]`);
+  Object.entries(fieldNameParamUTMMap).forEach(([fieldName, param]) => {
+    const values = queryParams.getAll(param);
+    const input = form.querySelector(`input[type=hidden][name=${fieldName}]`);
     if (values.length > 0 && input) {
       input.value = values.join();
     }
