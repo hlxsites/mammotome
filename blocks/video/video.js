ce067ba6fc6cd9b054b4315cc6c73ad23e7bdc67
Karl Pauls <karlpauls@gmail.com>
2023-07-10 17:39:46 +0200

    Video block should accept embed urls as well.

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -9,6 +9,7 @@ const HTML_PLAY_ICON = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 
   + '    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>\n'
   + '    <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>\n'
   + '</svg>';
+const YOUTUBE_URL_REGEX = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
 
 const getVideoPathFromVideo = (video) => {
   const videoURLElement = video.querySelector(':scope > div:nth-child(2) a');
@@ -20,12 +21,11 @@ const getVideoPathFromVideo = (video) => {
   if (!videoURLString) {
     return '';
   }
+  const matchUrl = videoURLString.match(YOUTUBE_URL_REGEX);
+  const videoCode = matchUrl && matchUrl[7];
 
-  const watchMode = '/watch?v=';
-  const watchIndex = videoURLString.indexOf(watchMode);
-
-  if (watchIndex > 0) {
-    return `/${videoURLString.substring(watchIndex + watchMode.length)}`;
+  if (videoCode && videoCode.length === 11) {
+    return `/${videoCode}`;
   }
 
   return new URL(videoURLString).pathname;
