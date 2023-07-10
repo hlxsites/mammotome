2851c3b99aaacd46c5890bc5d092732ec45aedc8
Pareesh Gupta <pareeshgupta@gmail.com>
2023-07-10 10:24:29 +0530

    Hidden fields for forms (#419)

---
+++ blocks/form/utm.js
@@ -0,0 +1,21 @@
+const fieldNameParamUTMMap = {
+  UTM_Campaign__c: 'utm_campaign',
+  UTM_Source__c: 'utm_source',
+  UTM_Medium__c: 'utm_medium',
+  UTM_Content__c: 'utm_content',
+  UTM_Term__c: 'utm_term',
+  GCLID__c: 'gclid',
+};
+
+export default async function decorate(form) {
+  const queryParams = new URLSearchParams(window.location.search);
+  Object.entries(fieldNameParamUTMMap).forEach(([fieldName, param]) => {
+    const values = queryParams.getAll(param);
+    if (values.length) {
+      const input = form.querySelector(`input[type=hidden][name="${fieldName}"]`);
+      if (input) {
+        input.value = values.join();
+      }
+    }
+  });
+}
