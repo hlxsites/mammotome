c05462802f7b7b17a8afa3502b0146a5c3f92b6b
davenichols-DHLS <124819426+davenichols-DHLS@users.noreply.github.com>
2024-01-12 10:04:41 +0000

    Update delayed.js
    Checking if the data-site would work

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,7 +4,7 @@ import { sampleRUM, fetchPlaceholders, loadScript } from './lib-franklin.js';
 document.dispatchEvent(new Event('franklin.delayed_begin'));
 
 // Fathom Analytics Code
-const attrsFa = JSON.parse('{"data-site": "MTMDHVUG"}');
+const attrsFa = {"data-site": "MTMDHVUG"};
 loadScript('https://cdn.usefathom.com/script.js', attrsFa);
 
 // Core Web Vitals RUM collection
