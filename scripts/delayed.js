244df02432c33baaf7a997b99e8262854de43b91
davenichols-DHLS <124819426+davenichols-DHLS@users.noreply.github.com>
2024-01-12 09:57:23 +0000

    Update delayed.js
    Add Fathom code to delayed instead

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,11 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM, fetchPlaceholders } from './lib-franklin.js';
+import { sampleRUM, fetchPlaceholders, loadScript } from './lib-franklin.js';
 
 document.dispatchEvent(new Event('franklin.delayed_begin'));
+
+// Fathom Analytics Code
+const attrsFa = JSON.parse('{"data-site": "MTMDHVUG"}');
+loadScript('https://cdn.usefathom.com/script.js', attrsFa);
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
