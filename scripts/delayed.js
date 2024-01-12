08b188a6677445cb548ee80db86bdb5fbdfee2b4
davenichols-DHLS <124819426+davenichols-DHLS@users.noreply.github.com>
2024-01-12 10:16:59 +0000

    Update delayed.js
    As lib-franklin is customised, then adding a new function to emulate this.

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,31 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM, fetchPlaceholders, loadScript } from './lib-franklin.js';
+import { sampleRUM, fetchPlaceholders } from './lib-franklin.js';
 
 document.dispatchEvent(new Event('franklin.delayed_begin'));
 
 // Fathom Analytics Code
+async function loadScriptFa(src, attrs) {
+  return new Promise((resolve, reject) => {
+    if (!document.querySelector(`head > script[src="${src}"]`)) {
+      const script = document.createElement('script');
+      script.src = src;
+      if (attrs) {
+        // eslint-disable-next-line no-restricted-syntax, guard-for-in
+        for (const attr in attrs) {
+          script.setAttribute(attr, attrs[attr]);
+        }
+      }
+      script.onload = resolve;
+      script.onerror = reject;
+      document.head.append(script);
+    } else {
+      resolve();
+    }
+  });
+}
+
 const attrsFa = {"data-site": "MTMDHVUG"};
-loadScript('https://cdn.usefathom.com/script.js', attrsFa);
+loadScriptFa('https://cdn.usefathom.com/script.js', attrsFa);
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
