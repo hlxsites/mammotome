ef3f51a0dbe74acdfa2d8e55577c9cd3a66a5d73
Krizel4 <krizelminnema@pm.me>
2026-04-22 12:55:40 -0700

    updated gtm script to run consent defaults immediately

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -414,31 +414,41 @@ function initMarketoEnhancedConversions() {
 
 // google tag manager
 function loadGTM() {
-  if (window.location.hostname.includes('localhost') || document.location.hostname.includes('.hlx.page') || document.location.hostname.includes('.aem.page')) {
+  if (window.location.hostname.includes('localhost') || 
+      document.location.hostname.includes('.hlx.page') || 
+      document.location.hostname.includes('.aem.page')) {
     return;
   }
 
-  const scriptTag = document.createElement('script');
-  scriptTag.innerHTML = `
-  // googleTagManager
-  (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({
-          'gtm.start':
-              new Date().getTime(), event: 'gtm.js'
-      });
-      var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src =
-          'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
-  })(window, document, 'script', 'dataLayer', 'GTM-KNBZTHP');
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  gtag('set', {
-      'cookie_flags': 'SameSite=None;Secure'
+  
+  gtag('consent', 'default', {
+    'ad_storage': 'denied',
+    'analytics_storage': 'denied',
+    'ad_user_data': 'denied',
+    'ad_personalization': 'denied',
+    'functionality_storage': 'denied',
+    'personalization_storage': 'denied',
+    'security_storage': 'granted',
+    'wait_for_update': 500
   });
+
+  const scriptTag = document.createElement('script');
+  scriptTag.innerHTML = `
+    (function(w,d,s,l,i){
+      w[l]=w[l]||[];
+      w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;
+      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KNBZTHP');
+    
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('set', {'cookie_flags': 'SameSite=None;Secure'});
   `;
   document.head.prepend(scriptTag);
 }
